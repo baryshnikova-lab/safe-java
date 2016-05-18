5900cc5704f6372fd645d65ec86f52b329f00b01
Jason Montojo <jrrmzz@gmail.com>
2016-05-18 08:26:16 -0400

    Added utility methods for computing colors.

--- core/src/main/java/edu/princeton/safe/internal/Util.java
+++ core/src/main/java/edu/princeton/safe/internal/Util.java
@@ -37,4 +37,53 @@ public class Util {
         return defaultPercentile.evaluate(values, threshold);
     }
 
+    public static int[] hslToRgb(double h,
+                                 double s,
+                                 double l) {
+        // Adapted from http://stackoverflow.com/a/9493060
+        double r;
+        double g;
+        double b;
+
+        if (s == 0) {
+            r = l;
+            g = l;
+            b = l;
+        } else {
+            double q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            double p = 2 * l - q;
+            r = hue2Rgb(p, q, h + 1.0 / 3);
+            g = hue2Rgb(p, q, h);
+            b = hue2Rgb(p, q, h - 1.0 / 3);
+        }
+        int[] result = { (int) Math.round(r * 255), (int) Math.round(g * 255), (int) Math.round(b * 255) };
+        return result;
+    }
+
+    private static double hue2Rgb(double p,
+                                  double q,
+                                  double t) {
+        if (t < 0)
+            t += 1;
+        if (t > 1)
+            t -= 1;
+        if (t < 1.0 / 6) {
+            return p + (q - p) * 6 * t;
+        }
+        if (t < 1.0 / 2) {
+            return q;
+        }
+        if (t < 2.0 / 3) {
+            return p + (q - p) * (2.0 / 3 - t) * 6;
+        }
+        return p;
+    }
+    
+    public static int[] interpolateLinear(int[] v1, int[] v2, double t) {
+        int[] result = new int[v1.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = (int) Math.round(v2[i] * t + v1[i] * (1 - t));
+        }
+        return result;
+    }
 }
